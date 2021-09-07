055e3c5b69f7b5dc3ec3e9e5625b785f910af05e
Corey McCourt <mccourtrc@gmail.com>
2021-09-07 08:38:47 -0700

    add theme types

---
+++ src/types/theme.ts
@@ -0,0 +1,21 @@
+export enum Color {
+  primary = '#E50914',
+  secondary = 'lightblue',
+  black = '#221f1f',
+}
+
+export enum Breakpoint {
+  sm = '@media(min-width: 640px)',
+  md = '@media(min-width: 768px)',
+  lg = '@media(min-width: 1024px)',
+  xl = '@media(min-width: 1280px)',
+  xxl = '@media(min-width: 1536px)',
+}
+
+export enum BP {
+  sm = Breakpoint.sm,
+  md = Breakpoint.md,
+  lg = Breakpoint.lg,
+  xl = Breakpoint.xl,
+  xxl = Breakpoint.xxl,
+}
