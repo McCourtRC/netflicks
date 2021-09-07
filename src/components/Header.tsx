00e72d5918f2bf44230bbaa8df5303e50b1b070e
Corey McCourt <mccourtrc@gmail.com>
2021-09-07 08:43:33 -0700

    Header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,23 @@
+import styled from '@emotion/styled';
 import React from 'react';
+import { Color } from 'types/theme';
+
+const Container = styled.header`
+  background-color: ${Color.black};
+`;
+
+const Logo = styled.h1`
+  margin: 0;
+  padding: 1rem;
+  color: ${Color.primary};
+`;
 
 const Header = () => {
-  return <header>Net Flicks</header>;
+  return (
+    <Container>
+      <Logo>Net Flicks</Logo>
+    </Container>
+  );
 };
 
 export default Header;
