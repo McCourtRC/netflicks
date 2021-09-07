326e488adf889afbba2596e9d57e0e3dd4d40b35
Corey McCourt <mccourtrc@gmail.com>
2021-09-07 08:01:45 -0700

    update baseUrl

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [tsConfigPaths(), reactRefresh()],
 });
