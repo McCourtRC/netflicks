b9ca5c6f4ca57bcb9e1e3416f7f47c6d77de9740
Corey McCourt <mccourtrc@gmail.com>
2021-09-08 00:50:36 -0700

    remove unused import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import tsConfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
