e4e0971618ec784137ea251f5c00a7e7785fe7bd
Corey McCourt <mccourtrc@gmail.com>
2021-09-08 01:17:01 -0700

    add apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './index.css';
 import App from './App';
 
+const client = new ApolloClient({
+  uri: 'https://graphql.us.fauna.com/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: `Bearer ${import.meta.env.VITE_FAUNA_ADMIN_KEY}`,
+  },
+});
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
