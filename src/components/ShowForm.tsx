1d773708927f6617c281329768e0c3865c5832de
Corey McCourt <mccourtrc@gmail.com>
2021-09-09 09:11:40 -0700

    ShowForm styles

--- src/components/ShowForm.tsx
+++ src/components/ShowForm.tsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { FormEvent, FormEventHandler } from 'react';
+import useHandleSubmit from 'hooks/useHandleSubmit';
+import useTextInput from 'hooks/useTextInput';
+import styled from '@emotion/styled';
+import { BoxShadow, Color } from 'types/theme';
+
+const Container = styled.div`
+  flex: 1;
+`;
+
+const Input = styled.input`
+  display: block;
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+  font-size: large;
+  border: none;
+  border-radius: 5px;
+  width: 50%;
+`;
+
+const Submit = styled.input`
+  border: none;
+  border-radius: 5px;
+  padding: 1rem;
+  color: white;
+  font-weight: bold;
+  background-color: ${Color.secondary};
+`;
 
 const ShowForm = () => {
-  return <div>Create Show</div>;
+  const [title, onChangeTitle, resetTitle] = useTextInput();
+  const [img, onChangeImg, resetImg] = useTextInput();
+  const handleSubmit = useHandleSubmit(() => {
+    console.log('HELLO');
+    // submit mutation
+    resetTitle();
+    resetImg();
+  });
+
+  return (
+    <Container>
+      <h1>New Show</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          type='text'
+          value={title}
+          placeholder='Title'
+          onChange={onChangeTitle}
+        />
+        <Input
+          type='text'
+          value={img}
+          placeholder='Image Url'
+          onChange={onChangeImg}
+        />
+        <Submit type='submit' />
+      </form>
+    </Container>
+  );
 };
 
 export default ShowForm;
