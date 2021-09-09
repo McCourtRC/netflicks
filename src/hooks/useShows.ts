532a9710cf16200b2bfcfdad9efeb8fb4dfcd4f6
Corey McCourt <mccourtrc@gmail.com>
2021-09-09 12:56:56 -0700

    move shows query to hook

---
+++ src/hooks/useShows.ts
@@ -0,0 +1,31 @@
+import { useQuery, gql } from '@apollo/client';
+import { Show } from 'types/show';
+
+interface Data {
+  allShows: {
+    data: Show[];
+  };
+}
+
+const GET_SHOWS = gql`
+  {
+    allShows {
+      data {
+        _id
+        name
+        asset {
+          image
+        }
+      }
+    }
+  }
+`;
+
+const useShows = () => {
+  const { data, ...rest } = useQuery<Data>(GET_SHOWS);
+  const shows = data?.allShows.data;
+
+  return { shows, ...rest };
+};
+
+export default useShows;
--- src/pages/Shows.tsx
+++ src/pages/Shows.tsx
@@ -1,30 +1,9 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
 import styled from '@emotion/styled';
 import { nth } from 'ramda';
 import ShowForm from 'components/ShowForm';
 import ShowList from 'components/ShowList';
-import { Show } from 'types/show';
-
-interface Data {
-  allShows: {
-    data: Show[];
-  };
-}
-
-const GET_SHOWS = gql`
-  {
-    allShows {
-      data {
-        _id
-        name
-        asset {
-          image
-        }
-      }
-    }
-  }
-`;
+import useShows from 'hooks/useShows';
 
 const Container = styled.div`
   display: flex;
@@ -34,8 +13,7 @@ const Container = styled.div`
 `;
 
 const Shows = () => {
-  const { data, loading, error } = useQuery<Data>(GET_SHOWS);
-  const shows = data?.allShows.data;
+  const { shows, loading, error } = useShows();
 
   if (error) return <div>{error}</div>;
   if (loading) return <div>loading...</div>;
